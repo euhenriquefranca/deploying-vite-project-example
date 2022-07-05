56e383ab4b0650922c0476404b74c3f5bf18b5d8
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2022-07-05 16:05:18 -0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <div style={{position: 'absolute'}}>
         <Music />
       </div>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Rotas/>
       </BrowserRouter>
     </div>
